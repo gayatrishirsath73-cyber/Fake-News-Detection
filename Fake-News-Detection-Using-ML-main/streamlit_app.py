b4146eaa190742cadfbde1e9b90c82493077feb7
Gayatri Shirsath <gayatrishirsath73@gmail.com>
2026-02-27 00:24:34 +0530

    Rename app.py to streamlit_app.py

---
+++ Fake-News-Detection-Using-ML-main/streamlit_app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pickle
+
+vectorization = pickle.load(open("vectorization.pkl", "rb"))
+model = pickle.load(open("LR_model.pkl", "rb"))
+
+st.title("Fake News Detection System")
+
+input_text = st.text_area("Enter News Text")
+
+if st.button("Predict"):
+    if input_text.strip() == "":
+        st.warning("Please enter some news text")
+    else:
+        vector_input = vectorization.transform([input_text])
+        prediction = model.predict(vector_input)
+
+        if prediction[0] == 1:
+            st.error("Fake News ")
+        else:
+            st.success("Real News ")
